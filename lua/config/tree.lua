5d766f7a7e2f85d0596276f52f6c7df7a172ae33
Rithul Kamesh <hi@rithul.dev>
2023-03-30 14:33:37 +0530

    Tree opens when opened directories

--- lua/config/tree.lua
+++ lua/config/tree.lua
@@ -9,6 +9,52 @@ local keymap = vim.api.nvim_set_keymap
 
 keymap("n", "<leader>e" ,":NvimTreeToggle<CR>", {silent=true})
 
+-- nvim-tree is also there in modified buffers so this function filter it out
+local modifiedBufs = function(bufs)
+    local t = 0
+    for k,v in pairs(bufs) do
+        if v.name:match("NvimTree_") == nil then
+            t = t + 1
+        end
+    end
+    return t
+end
+
+local function open_nvim_tree(data)
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- create a new, empty buffer
+  vim.cmd.enew()
+
+  -- wipe the directory buffer
+  vim.cmd.bw(data.buf)
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+        if #vim.api.nvim_list_wins() == 1 and
+        vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil and
+        modifiedBufs(vim.fn.getbufinfo({bufmodified = 1})) == 0 then
+            vim.cmd "quit"
+        end
+    end
+})
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
   renderer = {
