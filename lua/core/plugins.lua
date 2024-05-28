10e51c58ecd0d867fca65f6a4043801ec75e718d
Rithul Kamesh <hi@rithul.dev>
2024-05-28 18:34:18 +0530

    feat: update colorscheme

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,10 +1,9 @@
 return require("lazy").setup({
-
 	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
+		"rebelot/kanagawa.nvim",
+		config = function()
+			vim.cmd([[colorscheme kanagawa-wave]])
+		end,
 	},
 
 	-- Misc
