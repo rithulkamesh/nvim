80288fa5bf348b66664e0149c9439697141852c7
Rithul Kamesh <hi@rithul.dev>
2024-11-10 11:28:51 +0530

    feat: autocomplete on tab for comfort

--- lua/rithulkamesh/plugins/nvim-cmp.lua
+++ lua/rithulkamesh/plugins/nvim-cmp.lua
@@ -40,7 +40,7 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<Tab>"] = cmp.mapping.confirm({ select = true }),
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
